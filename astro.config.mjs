4d6ec186bf2550fb6c9a8c752e7526deb543d1b3
Louis Roehrs <lroehrs@gmail.com>
2026-04-12 03:31:21 -0700

    Case studies

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,11 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://teletrex.com',
+  vite: {
+    server: {
+      watch: {
+        ignored: ['**/#*.*#', '**/node-modules/**','**/temp-folder/**'], //
+      },
+    },
+  },
 })
